3d66381edb5738b3e17ee3fada2644592f6fca60
lu-26 <luiejohn.24@gmail.com>
2020-06-15 11:34:16 +0800

    added initial cards for project

--- src/components/projects/project.js
+++ src/components/projects/project.js
@@ -1,7 +1,81 @@
 import React from "react";
 
 import style from "./projects.module.scss";
+import user from "../../assets/img/user.jpg";
 
 export default function Project() {
-  return <div className={style.container}>asdsads</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.project}>
+        <div className={style.project_imgCont}>
+          {/* <img className={style.project_image} src={user} /> */}
+        </div>
+        <div className={style.project_desc}>The Cat Returns Drawing</div>
+
+        <div className={style.project_hiddenMenu}>
+          <div className={style.project_btn}>View</div>
+        </div>
+      </div>
+      <div className={style.project}>
+        <div className={style.project_imgCont}>
+          {/* <img className={style.project_image} src={user} /> */}
+        </div>
+        <div className={style.project_desc}>The Cat Returns Drawing</div>
+
+        <div className={style.project_hiddenMenu}>
+          <div className={style.project_btn}>View</div>
+        </div>
+      </div>
+      <div className={style.project}>
+        <div className={style.project_imgCont}>
+          {/* <img className={style.project_image} src={user} /> */}
+        </div>
+        <div className={style.project_desc}>The Cat Returns Drawing</div>
+
+        <div className={style.project_hiddenMenu}>
+          <div className={style.project_btn}>View</div>
+        </div>
+      </div>
+      <div className={style.project}>
+        <div className={style.project_imgCont}>
+          {/* <img className={style.project_image} src={user} /> */}
+        </div>
+        <div className={style.project_desc}>The Cat Returns Drawing</div>
+
+        <div className={style.project_hiddenMenu}>
+          <div className={style.project_btn}>View</div>
+        </div>
+      </div>
+      <div className={style.project}>
+        <div className={style.project_imgCont}>
+          {/* <img className={style.project_image} src={user} /> */}
+        </div>
+        <div className={style.project_desc}>The Cat Returns Drawing</div>
+
+        <div className={style.project_hiddenMenu}>
+          <div className={style.project_btn}>View</div>
+        </div>
+      </div>
+      <div className={style.project}>
+        <div className={style.project_imgCont}>
+          {/* <img className={style.project_image} src={user} /> */}
+        </div>
+        <div className={style.project_desc}>The Cat Returns Drawing</div>
+
+        <div className={style.project_hiddenMenu}>
+          <div className={style.project_btn}>View</div>
+        </div>
+      </div>
+      <div className={style.project}>
+        <div className={style.project_imgCont}>
+          {/* <img className={style.project_image} src={user} /> */}
+        </div>
+        <div className={style.project_desc}>The Cat Returns Drawing</div>
+
+        <div className={style.project_hiddenMenu}>
+          <div className={style.project_btn}>View</div>
+        </div>
+      </div>
+    </div>
+  );
 }
